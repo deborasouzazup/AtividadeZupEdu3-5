f645be50f8a1c1b10f67afdb918ffc5c1eef54fa
Debora Rodrigues <deboral.p@hotmail.com>
2022-08-23 17:55:39 -0300

    Implementar alert na View Controller

--- PlanoDeEstudoApp/ViewController.swift
+++ PlanoDeEstudoApp/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var areaDeAtuacaoTextFiedl: UITextField!
+    
+    @IBOutlet weak var statusTextFiedl: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
+    
+    @IBAction func ReceberMaterialButton(_ sender: UIButton) {
+        verificarDados()
+        
+    }
+    
+    func verificarDados() {
+        let alert = UIAlertController(title: "Quase lá", message: "Antes de enviarmos, por favor, revise seus dados:\nNome: \(nameTextField.text ?? "")\nEmail: \(emailTextField.text ?? "")\nÁrea de Atuação: \(areaDeAtuacaoTextFiedl.text ?? "")\nStatus Profissional: \(statusTextFiedl.text ?? "")", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Confirmar", style: .cancel, handler: acaoConfirmarSelecioanda))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func acaoConfirmarSelecioanda( _action: UIAlertAction) {
+        confirmarRecebimento()
+    }
+        
+    func confirmarRecebimento() {
+        let alert1 = UIAlertController(title: "Feito", message: "Verifique seu email e tenha acesso ao documento", preferredStyle: .alert)
+        
+        alert1.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+        
+        self.present(alert1, animated: true, completion: nil)
+    }    
 }
+
 
